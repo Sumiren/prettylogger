bc2273ef7fb3a8076dc125cf64de9b8de5009bfa
Sumiren <lotusumi0415@i.softbank.jp>
2018-08-20 21:01:31 +0900

    #1 support four log levels
    I implemented error, warn, and trace log levels additionally

--- PrettyLogger.Mock.Test/MockLogFormatterTest.cs
+++ PrettyLogger.Mock.Test/MockLogFormatterTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace PrettyLogger.Mock.Test
@@ -14,10 +15,21 @@ namespace PrettyLogger.Mock.Test
             log.WriteTo(implementation);
             Assert.That(log, Is.InstanceOf<IMockLog>());
             var mockLog = (IMockLog) log;
-            Assert.That(mockLog.LoggingArgument.LogLevel,         Is.EqualTo(LogLevel.Info));
+            Assert.That(mockLog.LoggingArgument.LogLevel,        Is.EqualTo(LogLevel.Info));
             Assert.That(mockLog.LoggingArgument.Timestamp.Value, Is.EqualTo(new DateTime(2000, 1, 1)));
             Assert.That(mockLog.LoggingArgument.Message,         Is.EqualTo("aiueo"));
             implementation.AssertLoggedOnlyOnce("aiueo");
+        }
+
+        [Test]
+        public void TestFormattedLogs()
+        {
+            var formatter = new MockLogFormatter();
+            var first = formatter.Format(new LoggingArgument(LogLevel.Info, new SimpleTimestamp(new DateTime(2000, 1, 1)), "aiueo"));
+            var last = formatter.Format(new LoggingArgument(LogLevel.Error, new SimpleTimestamp(new DateTime(2000, 1, 1)), "aiueo"));
+            Assert.That(formatter.FormattedLogs.Count, Is.EqualTo(2));
+            Assert.That(formatter.FormattedLogs.First(), Is.EqualTo(first));
+            Assert.That(formatter.FormattedLogs.Last(), Is.EqualTo(last));
         }
     }
 }
